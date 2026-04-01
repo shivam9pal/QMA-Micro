ee26a8c2efc0dec298c5e9abc4857b1462070193
shivam9pal <shivampaul9794@gmail.com>
2026-04-01 16:39:09 +0530

    added swagger

--- ApiGateway/src/main/java/com/apigateway/config/RouteValidator.java
+++ ApiGateway/src/main/java/com/apigateway/config/RouteValidator.java
@@ -1,10 +1,10 @@
 package com.apigateway.config;
 
-import org.springframework.http.server.reactive.ServerHttpRequest;
-import org.springframework.stereotype.Component;
-
 import java.util.List;
 import java.util.function.Predicate;
+
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
 
 @Component
 public class RouteValidator {
@@ -12,7 +12,13 @@ public class RouteValidator {
     public static final List<String> OPEN_ENDPOINTS = List.of(
             "/auth/signup",     // ✅ Matches your actual gateway route /auth/**
             "/auth/login",        // ✅ Matches your actual gateway route /auth/**
-            "/eureka"
+            "/eureka",
+            "/auth/signup",
+            "/auth/login",
+            "/eureka",
+            "/swagger-ui",
+            "/swagger-ui.html",
+            "/v3/api-docs"
     );
 
     public Predicate<ServerHttpRequest> isSecured =
